d384403721e29fb8c1c1f2f4abd719232cef31c1
Bessy Theodoropoulou <mellonance@gmail.com>
2017-09-26 22:29:49 +0300

    Add encryption and decryption methods

---
+++ RSA/EncryptorDecryptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace RSA
+{
+    class EncryptorDecryptor
+    {
+        public BigInteger c, m;
+
+        public void Encrypt(BigInteger m, BigInteger e, BigInteger n)          //encryption
+        {
+            c = BigInteger.ModPow(m, e, n);
+        }
+
+        public void Decrypt(BigInteger cResult, BigInteger d, BigInteger n)    //decryption
+        {
+            m = BigInteger.ModPow(cResult, d, n);
+        }
+    }
+}
